0694be5b90a821cc31e07d7c8de7566f5084149c
lopm00 <55919991+lopm00@users.noreply.github.com>
2020-02-10 14:23:13 +0100

    Update devise.js

--- devise.js
+++ devise.js
@@ -17,33 +17,46 @@ function Dollar() {
   } else {
     x.innerHTML = "4999 DH";
   }
-}
-
-
-function Dollar2() {
- 
   
-    var str = document.getElementById("convs3").innerHTML; 
-  var res = str.replace("399</span> DH", "39</span> $");
-  document.getElementById("convs3").innerHTML = res;
+      var x = document.getElementById("convs3");
+  if (x.innerHTML === "399 DH") {
+    x.innerHTML = "39 $";
+  } else {
+    x.innerHTML = "399 DH";
+  }
   
-    var str = document.getElementById("convs4").innerHTML; 
-  var res = str.replace("599</span> DH", "59</span> $");
-  document.getElementById("convs4").innerHTML = res;
+      var x = document.getElementById("convs4");
+  if (x.innerHTML === "599 DH") {
+    x.innerHTML = "59 $";
+  } else {
+    x.innerHTML = "599 DH";
+  }
   
-    var str = document.getElementById("convs5").innerHTML; 
-  var res = str.replace("999</span> DH", "99</span> $");
-  document.getElementById("convs5").innerHTML = res;
+      var x = document.getElementById("convs5");
+  if (x.innerHTML === "999 DH") {
+    x.innerHTML = "99 $";
+  } else {
+    x.innerHTML = "999 DH";
+  }
   
- var str = document.getElementById("convs6").innerHTML; 
-  var res = str.replace("799</span> DH", "80</span> $");
-  document.getElementById("convs6").innerHTML = res;
+      var x = document.getElementById("convs6");
+  if (x.innerHTML === "799 DH") {
+    x.innerHTML = "80 $";
+  } else {
+    x.innerHTML = "799 DH";
+  }
   
-  var str = document.getElementById("convs7").innerHTML; 
-  var res = str.replace("1999</span> DH", "200</span> $");
-  document.getElementById("convs7").innerHTML = res;
+      var x = document.getElementById("convs7");
+  if (x.innerHTML === "1999 DH") {
+    x.innerHTML = "200 $";
+  } else {
+    x.innerHTML = "1999 DH";
+  }
   
-  var str = document.getElementById("convs8").innerHTML; 
-  var res = str.replace("3999</span> DH", "399</span> $");
-  document.getElementById("convs8").innerHTML = res;
+      var x = document.getElementById("convs8");
+  if (x.innerHTML === "3999 DH") {
+    x.innerHTML = "399 $";
+  } else {
+    x.innerHTML = "3999 DH";
+  }
 }
