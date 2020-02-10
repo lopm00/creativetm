0f82c68082cc31305f6986093012c4f76fc1698c
lopm00 <55919991+lopm00@users.noreply.github.com>
2020-02-10 13:01:28 +0100

    Update devise.js

--- devise.js
+++ devise.js
@@ -2,4 +2,36 @@ function myFunction() {
   var str = document.getElementById("convs").innerHTML; 
   var res = str.replace("999</span> DH", "99</span> $");
   document.getElementById("convs").innerHTML = res;
+  
+  var str = document.getElementById("convs1").innerHTML; 
+  var res = str.replace("1499</span> DH", "155</span> $");
+  document.getElementById("convs1").innerHTML = res;
+  
+  var str = document.getElementById("convs2").innerHTML; 
+  var res = str.replace("4999</span> DH", "499</span> $");
+  document.getElementById("convs2").innerHTML = res;
+  
+    var str = document.getElementById("convs3").innerHTML; 
+  var res = str.replace("399</span> DH", "39</span> $");
+  document.getElementById("convs3").innerHTML = res;
+  
+    var str = document.getElementById("convs4").innerHTML; 
+  var res = str.replace("599</span> DH", "59</span> $");
+  document.getElementById("convs4").innerHTML = res;
+  
+    var str = document.getElementById("convs5").innerHTML; 
+  var res = str.replace("999</span> DH", "99</span> $");
+  document.getElementById("convs5").innerHTML = res;
+  
+ var str = document.getElementById("convs6").innerHTML; 
+  var res = str.replace("799</span> DH", "80</span> $");
+  document.getElementById("convs6").innerHTML = res;
+  
+  var str = document.getElementById("convs7").innerHTML; 
+  var res = str.replace("1999</span> DH", "200</span> $");
+  document.getElementById("convs7").innerHTML = res;
+  
+  var str = document.getElementById("convs8").innerHTML; 
+  var res = str.replace("3999</span> DH", "399</span> $");
+  document.getElementById("convs8").innerHTML = res;
 }
