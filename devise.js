b8eff98753bf8f264dc72ced8cc185c179cf91c9
lopm00 <55919991+lopm00@users.noreply.github.com>
2020-02-10 20:08:26 +0100

    Update devise.js

--- devise.js
+++ devise.js
@@ -60,3 +60,130 @@ function Dollar() {
     x.innerHTML = "3999 DH";
   }
 }
+
+function Mad() {
+  var x = document.getElementById("convs");
+  if (x.innerHTML === "999 DH") {
+    x.innerHTML = "999 DH";
+  } else {
+    x.innerHTML = "999 DH";
+  }
+    var x = document.getElementById("convs1");
+  if (x.innerHTML === "1499 DH") {
+    x.innerHTML = "1499 DH";
+  } else {
+    x.innerHTML = "1499 DH";
+  }
+    var x = document.getElementById("convs2");
+  if (x.innerHTML === "4999 DH") {
+    x.innerHTML = "4999 DH";
+  } else {
+    x.innerHTML = "4999 DH";
+  }
+  
+      var x = document.getElementById("convs3");
+  if (x.innerHTML === "399 DH") {
+    x.innerHTML = "399 DH";
+  } else {
+    x.innerHTML = "399 DH";
+  }
+  
+      var x = document.getElementById("convs4");
+  if (x.innerHTML === "599 DH") {
+    x.innerHTML = "599 DH";
+  } else {
+    x.innerHTML = "599 DH";
+  }
+  
+      var x = document.getElementById("convs5");
+  if (x.innerHTML === "999 DH") {
+    x.innerHTML = "999 DH";
+  } else {
+    x.innerHTML = "999 DH";
+  }
+  
+      var x = document.getElementById("convs6");
+  if (x.innerHTML === "799 DH") {
+    x.innerHTML = "799 DH";
+  } else {
+    x.innerHTML = "799 DH";
+  }
+  
+      var x = document.getElementById("convs7");
+  if (x.innerHTML === "1999 DH") {
+    x.innerHTML = "1999 DH";
+  } else {
+    x.innerHTML = "1999 DH";
+  }
+  
+      var x = document.getElementById("convs8");
+  if (x.innerHTML === "3999 DH") {
+    x.innerHTML = "3999 DH";
+  } else {
+    x.innerHTML = "3999 DH";
+  }
+}
+
+function Euro() {
+  var x = document.getElementById("convs");
+  if (x.innerHTML === "999 DH") {
+    x.innerHTML = "89 €";
+  } else {
+    x.innerHTML = "999 DH";
+  }
+    var x = document.getElementById("convs1");
+  if (x.innerHTML === "1499 DH") {
+    x.innerHTML = "139 €";
+  } else {
+    x.innerHTML = "1499 DH";
+  }
+    var x = document.getElementById("convs2");
+  if (x.innerHTML === "4999 DH") {
+    x.innerHTML = "470 €";
+  } else {
+    x.innerHTML = "4999 DH";
+  }
+  
+      var x = document.getElementById("convs3");
+  if (x.innerHTML === "399 DH") {
+    x.innerHTML = "37 €";
+  } else {
+    x.innerHTML = "399 DH";
+  }
+  
+      var x = document.getElementById("convs4");
+  if (x.innerHTML === "599 DH") {
+    x.innerHTML = "56 €";
+  } else {
+    x.innerHTML = "599 DH";
+  }
+  
+      var x = document.getElementById("convs5");
+  if (x.innerHTML === "999 DH") {
+    x.innerHTML = "94 €";
+  } else {
+    x.innerHTML = "999 DH";
+  }
+  
+      var x = document.getElementById("convs6");
+  if (x.innerHTML === "799 DH") {
+    x.innerHTML = "75 €";
+  } else {
+    x.innerHTML = "799 DH";
+  }
+  
+      var x = document.getElementById("convs7");
+  if (x.innerHTML === "1999 DH") {
+    x.innerHTML = "189 €";
+  } else {
+    x.innerHTML = "1999 DH";
+  }
+  
+      var x = document.getElementById("convs8");
+  if (x.innerHTML === "3999 DH") {
+    x.innerHTML = "378 €";
+  } else {
+    x.innerHTML = "3999 DH";
+  }
+}
+
